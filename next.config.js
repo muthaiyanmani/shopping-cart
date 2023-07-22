1342c99c8d2ed0b33cc22d1bff6c40f6c235604c
Muthaiyan Mani <muthaiyan.mb@zohocorp.com>
2023-07-22 14:04:55 +0530

    feat: added header, footer and homepage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.zylker.com',
+        port: ''
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
