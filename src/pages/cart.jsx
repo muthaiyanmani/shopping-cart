a13e44ef15e8f1d08f77932f404f4049873f76f5
Muthaiyan Mani <muthaiyan.mb@zohocorp.com>
2023-07-22 21:18:38 +0530

    feat: added checkout page

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,8 +1,9 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-import Products from "@/components/products";
 import { useCart } from "@/context/cart-context";
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Cart() {
   const { products, removeProductFromCart } = useCart();
@@ -10,49 +11,125 @@ export default function Cart() {
     <>
       <Header />
       <h2 className="px-4 my-4 text-xl font-bold">My Cart</h2>
-      <div className="grid grid-cols-1 mt-8 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-      
+      <div >
         {products?.length ? (
-          products.map((product) => (
-            <div key={product.id}>
-              <div className="relative cursor-pointer">
-                <div className="relative w-full overflow-hidden rounded-lg h-72">
-                  <Image
-                    height={300}
-                    width={300}
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
-                    className="object-cover object-center w-full h-full"
-                  />
+          <div className="flex justify-evenly">
+            <div className="grid grid-cols-1 mt-8 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-2 xl:gap-x-8">
+              {products.map((product) => (
+                <div key={product.id}>
+                  <div className="relative cursor-pointer">
+                    <div className="relative w-full overflow-hidden rounded-lg h-72">
+                      <Image
+                        height={300}
+                        width={300}
+                        src={product.imageSrc}
+                        alt={product.imageAlt}
+                        className="object-cover object-center w-full h-full"
+                      />
+                    </div>
+                    <div className="relative mt-4">
+                      <h3 className="text-sm font-medium text-gray-900">
+                        {product.name}
+                      </h3>
+                      <p className="mt-1 text-sm text-gray-500">
+                        {product.color}
+                      </p>
+                    </div>
+                    <div className="absolute inset-x-0 top-0 flex items-end justify-end p-4 overflow-hidden rounded-lg h-72">
+                      <div
+                        aria-hidden="true"
+                        className="absolute inset-x-0 bottom-0 opacity-50 h-36 bg-gradient-to-t from-black"
+                      />
+                      <p className="relative text-lg font-semibold text-white">
+                        {product.price}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="mt-6">
+                    <button
+                      type="button"
+                      onClick={() => removeProductFromCart(product.id)}
+                      className="relative flex items-center justify-center w-full px-8 py-2 text-sm font-medium text-gray-900 bg-gray-100 border-2 border-transparent border-gray-400 rounded-md hover:bg-gray-200"
+                    >
+                      Remove from bag
+                      <span className="sr-only">, {product.name}</span>
+                    </button>
+                  </div>
                 </div>
-                <div className="relative mt-4">
-                  <h3 className="text-sm font-medium text-gray-900">
-                    {product.name}
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                </div>
-                <div className="absolute inset-x-0 top-0 flex items-end justify-end p-4 overflow-hidden rounded-lg h-72">
-                  <div
-                    aria-hidden="true"
-                    className="absolute inset-x-0 bottom-0 opacity-50 h-36 bg-gradient-to-t from-black"
-                  />
-                  <p className="relative text-lg font-semibold text-white">
-                    {product.price}
-                  </p>
-                </div>
-              </div>
-              <div className="mt-6">
-                <button
-                  type="button"
-                  onClick={() => removeProductFromCart(product.id)}
-                  className="relative flex items-center justify-center w-full px-8 py-2 text-sm font-medium text-gray-900 bg-gray-100 border-2 border-transparent border-gray-400 rounded-md hover:bg-gray-200"
-                >
-                  Remove from bag
-                  <span className="sr-only">, {product.name}</span>
-                </button>
-              </div>
+              ))}
             </div>
-          ))
+
+            <section
+              aria-labelledby="summary-heading"
+              className="px-4 py-6 mt-16 rounded-lg w-96 bg-gray-50 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
+            >
+              <h2
+                id="summary-heading"
+                className="text-lg font-medium text-gray-900"
+              >
+                Order summary
+              </h2>
+
+              <dl className="mt-6 space-y-4">
+                <div className="flex items-center justify-between">
+                  <dt className="text-sm text-gray-600">Subtotal</dt>
+                  <dd className="text-sm font-medium text-gray-900">$99.00</dd>
+                </div>
+                <div className="flex items-center justify-between pt-4 border-t border-gray-200">
+                  <dt className="flex items-center text-sm text-gray-600">
+                    <span>Shipping estimate</span>
+                    <a
+                      href="#"
+                      className="flex-shrink-0 ml-2 text-gray-400 hover:text-gray-500"
+                    >
+                      <span className="sr-only">
+                        Learn more about how shipping is calculated
+                      </span>
+                      <QuestionMarkCircleIcon
+                        className="w-5 h-5"
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </dt>
+                  <dd className="text-sm font-medium text-gray-900">$5.00</dd>
+                </div>
+                <div className="flex items-center justify-between pt-4 border-t border-gray-200">
+                  <dt className="flex text-sm text-gray-600">
+                    <span>Tax estimate</span>
+                    <a
+                      href="#"
+                      className="flex-shrink-0 ml-2 text-gray-400 hover:text-gray-500"
+                    >
+                      <span className="sr-only">
+                        Learn more about how tax is calculated
+                      </span>
+                      <QuestionMarkCircleIcon
+                        className="w-5 h-5"
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </dt>
+                  <dd className="text-sm font-medium text-gray-900">$8.32</dd>
+                </div>
+                <div className="flex items-center justify-between pt-4 border-t border-gray-200">
+                  <dt className="text-base font-medium text-gray-900">
+                    Order total
+                  </dt>
+                  <dd className="text-base font-medium text-gray-900">
+                    $112.32
+                  </dd>
+                </div>
+              </dl>
+
+              <div className="mt-10">
+                <Link href="/checkout"
+                  className="inline-block px-4 py-3 text-sm font-medium text-center text-white bg-blue-600 border border-transparent rounded-md shadow-sm w-80 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+                >
+                  Checkout
+                </Link>
+              </div>
+            </section>
+          </div>
         ) : (
           <h3 className="text-2xl font-bold text-center">No items in cart</h3>
         )}
---
+++ src/pages/checkout.jsx
@@ -0,0 +1,467 @@
+import { Fragment, useState } from "react";
+import {
+  Dialog,
+  Popover,
+  RadioGroup,
+  Tab,
+  Transition
+} from "@headlessui/react";
+import {
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  QuestionMarkCircleIcon,
+  ShoppingBagIcon,
+  XMarkIcon
+} from "@heroicons/react/24/outline";
+import { CheckCircleIcon } from "@heroicons/react/20/solid";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import axios from "axios";
+import Link from "next/link";
+
+const deliveryMethods = [
+  {
+    id: 1,
+    title: "Standard",
+    turnaround: "4–10 business days",
+    price: "$5.00"
+  },
+  { id: 2, title: "Express", turnaround: "2–5 business days", price: "$16.00" }
+];
+const paymentMethods = [
+  { id: "credit-card", title: "Credit card" },
+  { id: "paypal", title: "PayPal" },
+  { id: "etransfer", title: "eTransfer" }
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Example() {
+  const [email, setEmail] = useState("");
+  const [selectedDeliveryMethod, setSelectedDeliveryMethod] = useState(
+    deliveryMethods[0]
+  );
+
+  let [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  const submitOrder = async (e) => {
+    e.preventDefault();
+    // const { data } = await axios.get(`/api/mail?email=${email}`);
+    setIsOpen(true);
+  };
+
+  return (
+    <div>
+      <Header />
+      <main className="px-4 pt-16 pb-24 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="max-w-2xl mx-auto lg:max-w-none">
+          <h1 className="sr-only">Checkout</h1>
+
+          <form className="lg:grid lg:grid-cols-2 lg:gap-x-12 xl:gap-x-16">
+            <div>
+              <div>
+                <h2 className="text-lg font-medium text-gray-900">
+                  Contact information
+                </h2>
+
+                <div className="mt-4">
+                  <label
+                    htmlFor="email-address"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Email address
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="email"
+                      id="email-address"
+                      name="email-address"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      autoComplete="email"
+                      className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="pt-10 mt-10 border-t border-gray-200">
+                <h2 className="text-lg font-medium text-gray-900">
+                  Shipping information
+                </h2>
+
+                <div className="grid grid-cols-1 mt-4 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
+                  <div>
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      First name
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        id="first-name"
+                        name="first-name"
+                        autoComplete="given-name"
+                        className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="last-name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Last name
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        id="last-name"
+                        name="last-name"
+                        autoComplete="family-name"
+                        className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="address"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Address
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        name="address"
+                        id="address"
+                        autoComplete="street-address"
+                        className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="region"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      State / Province
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        name="region"
+                        id="region"
+                        autoComplete="address-level1"
+                        className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="postal-code"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Postal code
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        name="postal-code"
+                        id="postal-code"
+                        autoComplete="postal-code"
+                        className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="phone"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Phone
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        name="phone"
+                        id="phone"
+                        autoComplete="tel"
+                        className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="">
+                <RadioGroup
+                  value={selectedDeliveryMethod}
+                  onChange={setSelectedDeliveryMethod}
+                >
+                  <RadioGroup.Label className="text-lg font-medium text-gray-900">
+                    Delivery method
+                  </RadioGroup.Label>
+
+                  <div className="grid grid-cols-1 mt-4 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
+                    {deliveryMethods.map((deliveryMethod) => (
+                      <RadioGroup.Option
+                        key={deliveryMethod.id}
+                        value={deliveryMethod}
+                        className={({ checked, active }) =>
+                          classNames(
+                            checked ? "border-transparent" : "border-gray-300",
+                            active ? "ring-2 ring-blue-500" : "",
+                            "relative flex cursor-pointer rounded-lg border bg-white p-4 shadow-sm focus:outline-none"
+                          )
+                        }
+                      >
+                        {({ checked, active }) => (
+                          <>
+                            <span className="flex flex-1">
+                              <span className="flex flex-col">
+                                <RadioGroup.Label
+                                  as="span"
+                                  className="block text-sm font-medium text-gray-900"
+                                >
+                                  {deliveryMethod.title}
+                                </RadioGroup.Label>
+                                <RadioGroup.Description
+                                  as="span"
+                                  className="flex items-center mt-1 text-sm text-gray-500"
+                                >
+                                  {deliveryMethod.turnaround}
+                                </RadioGroup.Description>
+                                <RadioGroup.Description
+                                  as="span"
+                                  className="mt-6 text-sm font-medium text-gray-900"
+                                >
+                                  {deliveryMethod.price}
+                                </RadioGroup.Description>
+                              </span>
+                            </span>
+                            {checked ? (
+                              <CheckCircleIcon
+                                className="w-5 h-5 text-blue-600"
+                                aria-hidden="true"
+                              />
+                            ) : null}
+                            <span
+                              className={classNames(
+                                active ? "border" : "border-2",
+                                checked
+                                  ? "border-blue-500"
+                                  : "border-transparent",
+                                "pointer-events-none absolute -inset-px rounded-lg"
+                              )}
+                              aria-hidden="true"
+                            />
+                          </>
+                        )}
+                      </RadioGroup.Option>
+                    ))}
+                  </div>
+                </RadioGroup>
+              </div>
+
+              {/* Payment */}
+              <div className="pt-10 mt-10 border-t border-gray-200">
+                <h2 className="text-lg font-medium text-gray-900">Payment</h2>
+
+                <fieldset className="mt-4">
+                  <legend className="sr-only">Payment type</legend>
+                  <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
+                    {paymentMethods.map((paymentMethod, paymentMethodIdx) => (
+                      <div key={paymentMethod.id} className="flex items-center">
+                        {paymentMethodIdx === 0 ? (
+                          <input
+                            id={paymentMethod.id}
+                            name="payment-type"
+                            type="radio"
+                            defaultChecked
+                            className="w-4 h-4 text-blue-600 border-gray-300 focus:ring-blue-500"
+                          />
+                        ) : (
+                          <input
+                            id={paymentMethod.id}
+                            name="payment-type"
+                            type="radio"
+                            className="w-4 h-4 text-blue-600 border-gray-300 focus:ring-blue-500"
+                          />
+                        )}
+
+                        <label
+                          htmlFor={paymentMethod.id}
+                          className="block ml-3 text-sm font-medium text-gray-700"
+                        >
+                          {paymentMethod.title}
+                        </label>
+                      </div>
+                    ))}
+                  </div>
+                </fieldset>
+
+                <div className="grid grid-cols-4 mt-6 gap-x-4 gap-y-6">
+                  <div className="col-span-2">
+                    <label
+                      htmlFor="card-number"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Card number
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        id="card-number"
+                        name="card-number"
+                        autoComplete="cc-number"
+                        className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-span-2">
+                    <label
+                      htmlFor="name-on-card"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Expiration date (MM/YY)
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        id="name-on-card"
+                        name="name-on-card"
+                        autoComplete="cc-name"
+                        className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-span-3">
+                    <label
+                      htmlFor="expiration-date"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Name on card
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        name="expiration-date"
+                        id="expiration-date"
+                        autoComplete="cc-exp"
+                        className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="cvc"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      CVC
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        name="cvc"
+                        id="cvc"
+                        autoComplete="csc"
+                        className="block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-span-4 mt-2">
+                    <button
+                      type="submit"
+                      onClick={submitOrder}
+                      className="w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+                    >
+                      Confirm order
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </main>
+      <Footer />
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex items-center justify-center min-h-full p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Order successfully placed!
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      Your payment has been successfully submitted. We’ve sent
+                      you an email with all of the details of your order.
+                    </p>
+                  </div>
+
+                  <div className="mt-6">
+                    <Link href="/"
+                      className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                    >
+                      Redirect to Home
+                    </Link>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </div>
+  );
+}
